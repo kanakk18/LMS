11847c114e1c302eb213d61c7eabfab9db4249f9
kanakk18 <kanaksingh18aug@gmail.com>
2025-05-15 11:29:17 +0530

    v

--- server/models/Purchase.js
+++ server/models/Purchase.js
@@ -4,6 +4,7 @@ import Course from "./Course.js";
 const PurchaseSchema = new mongoose.Schema({
     CourseId: { type: mongoose.Schema.Types.ObjectId, ref: 'Course', required: true },
     userId: { type: String, ref: 'User', required: true },
+    
     amount: { type: Number, required: true },
     status: { type: String, enum: ['pending', 'completed', 'failed'], default: 'pending' }
 }, { timestamps: true });
