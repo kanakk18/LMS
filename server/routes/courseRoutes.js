636acca7753e70e361aa8d18336c480212336985
kanakk18 <kanaksingh18aug@gmail.com>
2025-05-15 12:12:47 +0530

    fix: correct function name for retrieving all courses

--- server/routes/courseRoutes.js
+++ server/routes/courseRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getAllCourses, getCourseId } from '../controllers/courseController.js';
 const courseRouter = express.Router()
-courseRouter.get('/all' , getAllCourse)
+courseRouter.get('/all' , getAllCourses)
 courseRouter.get('/:id' , getCourseId)  
  export default courseRouter;
