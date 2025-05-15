495fa0ba158f70d3fa63bac54beff45945cb506e
kanakk18 <kanaksingh18aug@gmail.com>
2025-05-15 16:56:03 +0530

    fh

--- server/models/Course.js
+++ server/models/Course.js
@@ -36,6 +36,7 @@ const courseSchema = new mongoose.Schema({
     ],
 }, { timestamps: true, minimize: false });
 
-const Course = mongoose.model('Course', courseSchema);
+const Course = mongoose.models.Course || mongoose.model('Course', courseSchema);
 export default Course;
+
 
